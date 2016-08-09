7655d35fd66ca07bae9f33d1eda6dcdf1097d367
priscilarosa <priscilarosa@MacBook-Pro-de-Priscila.local>
2016-08-09 16:05:09 -0300

    askHelpViewController

---
+++ HelpMiga/AskHelpViewController.swift
@@ -0,0 +1,67 @@
+//
+//  AskHelpViewController.swift
+//  HelpMiga
+//
+//  Created by Priscila Rosa on 8/9/16.
+//  Copyright © 2016 Guilherme Marques. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class AskHelpViewController: UIViewController, MKMapViewDelegate {
+    
+    var locationManager = CLLocationManager()
+    let regionRadius: CLLocationDistance = 1000
+    let initialLocation = CLLocation()
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBAction func askHelpButton(sender: AnyObject) {
+    }
+
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        checkLocationAuthorizationStatus()
+//        centerMapOnLocation(initialLocation)
+        
+        mapView.showsUserLocation = true
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestAlwaysAuthorization()
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.mapView.setRegion(region, animated: true)
+    }
+
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+
+
+
+}
