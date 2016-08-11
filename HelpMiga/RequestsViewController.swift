dc4bf093ec8b8fde4ef8c864d8d3b213a401c7d3
priscilarosa <priscilarosa@MacBook-Pro-de-Priscila.local>
2016-08-11 20:01:44 -0300

    request view controller

--- HelpMiga/RequestsViewController.swift
+++ HelpMiga/RequestsViewController.swift
@@ -16,20 +16,42 @@ class RequestsViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var requestedHelpName: UILabel!
     @IBOutlet weak var requestedHelpDistance: UILabel!
     @IBOutlet weak var requestedHelpMapView: MKMapView!
+    @IBOutlet weak var requestView: UIView!
+    
+    @IBOutlet weak var noRequestsLabel: UILabel!
+    @IBOutlet weak var logo: UIImageView!
     
     @IBAction func callRequestedHelpButton(sender: AnyObject) {
     }
     
     @IBAction func goingButton(sender: AnyObject) {
+        
+        guard let image = UIImage(named: "goingSelected") else {
+            print("Image Not Found")
+            return
+        }
+        sender.setBackgroundImage(image, forState: UIControlState.Normal)
     }
 
     @IBAction func rejectRequestButton(sender: AnyObject) {
+        requestView.hidden = true
+        noRequestsLabel.hidden = false
+        logo.hidden = false
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //comentar as 3 linhas abaixo pra aparecer a view do request
+//        requestView.hidden = true
+//        noRequestsLabel.hidden = false
+//        logo.hidden = false
 
-        requestsBG.layer.cornerRadius = 50
-        requestsBG.clipsToBounds = true
+        requestedHelpImageView.layer.cornerRadius = requestedHelpImageView.frame.size.width/2
+        requestedHelpImageView.clipsToBounds = true
+        requestedHelpImageView.layer.borderWidth = 3
+        requestedHelpImageView.layer.borderColor = UIColor.whiteColor().CGColor
+        requestedHelpMapView.layer.cornerRadius = 10
+        requestedHelpMapView.clipsToBounds = true
     }
 
     override func didReceiveMemoryWarning() {
