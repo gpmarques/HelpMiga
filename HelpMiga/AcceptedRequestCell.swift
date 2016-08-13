534a967b174f46cca30ce3ce9d4003a0a8b777f7
priscilarosa <priscilarosa@MacBook-Pro-de-Priscila.local>
2016-08-13 19:01:35 -0300

    ajustes

---
+++ HelpMiga/AcceptedRequestCell.swift
@@ -0,0 +1,34 @@
+//
+//  AcceptedRequestCell.swift
+//  HelpMiga
+//
+//  Created by Priscila Rosa on 8/13/16.
+//  Copyright © 2016 Guilherme Marques. All rights reserved.
+//
+
+import UIKit
+
+class AcceptedRequestCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var distance: UILabel!
+    
+    @IBOutlet weak var whiteView: UIView!
+    
+    @IBAction func callButton(sender: AnyObject) {
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        image.layer.cornerRadius = image.frame.size.width / 2;
+        image.clipsToBounds = true
+        image.layer.borderWidth = 3
+        image.layer.borderColor = UIColor.whiteColor().CGColor
+        whiteView.layer.cornerRadius = 10
+    }
+    
+}
